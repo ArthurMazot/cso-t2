db567d801a5b97f4053540b6d74d7f28030eef52
GiovannaBorba <143823107+GiovannaBorba@users.noreply.github.com>
2025-05-15 17:32:53 -0300

    Update test_mq_driver.c

--- test_mq_driver.c
+++ test_mq_driver.c
@@ -14,38 +14,37 @@ int main(){
     if(fd < 0)
         return 1;
 
-    if(write(fd, "1 nomeQualquer\0", strlen("1 nomeQualquer\0"))){
-        printf("Adicionado\n");
+    while (1) {
+        printf("\n> ");
+        fflush(stdout);
+        memset(buff, 0, SIZE_BUFF);
+        fgets(buff, SIZE_BUFF, stdin);
+        buff[strcspn(buff, "\n")] = 0;  // Remove '\n'
+
+        if (strncmp(buff, "/exit", 5) == 0) {
+            break;
+
+        } else if (strncmp(buff, "/read", 5) == 0) {
+            read(fd, "", strlen("")); 
+
+        } else if (strncmp(buff, "/reg ", 5) == 0) {
+            snprintf(buff, SIZE_BUFF, "1 %s", buff + 5);
+            write(fd, buff, strlen(buff));
+
+        } else if (strncmp(buff, "/unreg ", 7) == 0) {
+            snprintf(buff, SIZE_BUFF, "4 %s", buff + 7);
+            write(fd, buff, strlen(buff)); 
+
+        } else if (strncmp(buff, "/[all] ", 7) == 0) {
+            snprintf(buff, SIZE_BUFF, "3 %s", buff + 7);
+            write(fd, buff, strlen(buff)); 
+
+        } else if (buff[0] == '/' && buff[1] != '[') {
+                snprintf(buff, SIZE_BUFF, "2 %s", buff + 1);
+                write(fd, buff, strlen(buff));  
+        }
     }
-    else return 0;
-
-    fgets(buff, SIZE_BUFF, stdin);
-
-    if(write(fd, "3 Teste de msg 1\0", strlen("3 Teste de msg 1\0"))){
-        printf("Teste mandado\n");
-    }
-
-    if(write(fd, "3 Teste de msg 2\0", strlen("3 Teste de msg 2\0"))){
-        printf("Teste mandado\n");
-    }
-
-    if(write(fd, "3 Teste de msg 3\0", strlen("3 Teste de msg 3\0"))){
-        printf("Teste mandado\n");
-    }
-
-    if(write(fd, "3 Teste de msg 4\0", strlen("3 Teste de msg 4\0"))){
-        printf("Teste mandado\n");
-    }
-
-    fgets(buff, SIZE_BUFF, stdin);
-
-    if(read(fd, "", strlen("")));
-    if(read(fd, "", strlen("")));
-    if(read(fd, "", strlen("")));
-    if(read(fd, "", strlen("")));
-
-
-    write(fd, "4 nomeQualquer\0", strlen("4 nomeQualquer\0"));
 
     close(fd);
+    return 0;
 }
